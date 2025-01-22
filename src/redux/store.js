46c3f2e3882418bc950847a4a6dbc2bceda525ec
Betul <betul@users.noreply.github.com>
2025-01-22 12:11:08 +0300

    Add Redux Persist for state persistence across reloads.

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,14 +1,39 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import { combineReducers } from "redux";
 import moviesReducer from "./moviesSlice";
 import movieDetailsReducer from "./movieDetailsSlice";
 import navigationReducer from "./navigationSlice";
 
-const store = configureStore({
-  reducer: {
-    movies: moviesReducer,
-    movieDetails: movieDetailsReducer,
-    navigation: navigationReducer,
-  },
+// Root Reducer
+const rootReducer = combineReducers({
+  movies: moviesReducer,
+  movieDetails: movieDetailsReducer,
+  navigation: navigationReducer,
 });
+
+// Persist Config
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+// Persisted Reducer
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+// Store
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: ["persist/PERSIST", "persist/REHYDRATE"],
+      },
+    }),
+});
+
+// Persistor
+export const persistor = persistStore(store);
 
 export default store;
